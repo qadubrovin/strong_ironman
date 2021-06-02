72ce6eaa0e2e9449c805e467a7bf78fa97ed5785
qadubrovin <qadubrovin@yandex.ru>
2021-06-02 23:00:56 +0300

    change locators

--- src/test/java/pages/StudentRegistrationPage.java
+++ src/test/java/pages/StudentRegistrationPage.java
@@ -1,47 +1,47 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
-public class StudentRegistrationPage {
+public class StudentRegistrationPage<nameColumn> {
 
     String monthOfBirth = "August",
             yearOfBirth = "1988";
 
 
     //selectors for fill form
-    public String otherGender = "//*[@id='genterWrapper']//*[@for='gender-radio-3']",
-            name = "#firstName",
-            familyName = "//*[@id='lastName']",
-            userEmail = "#userEmail",
-            userNumber = "#userNumber",
-            subjectInput = "#subjectsInput",
-            submitButton = "#submit",
-            hobbySports = "//*[contains(text(),'Sports')]",
-            uploadPicture = "#uploadPicture",
-            usersAddress = "#currentAddress";
+    public SelenideElement otherGender = $x("//*[@for='gender-radio-3']"),
+            name = $("#firstName"),
+            familyName = $x("//*[@id='lastName']"),
+            userEmail = $("#userEmail"),
+            userNumber = $("#userNumber"),
+            subjectInput = $("#subjectsInput"),
+            submitButton = $("#submit"),
+            hobbySports = $x("//*[contains(text(),'Sports')]"),
+            uploadPicture = $("#uploadPicture"),
+            usersAddress = $("#currentAddress");
 
     //selectors for success form
-    public String successHeader = "#example-modal-sizes-title-lg",
-            nameColumn = "//*[contains(text(),'Student Name')]//..",
-            studentEmailColumn = "//*[contains(text(),'Student Email')]//..",
-            studentGenderColumn = "//*[@class='table-responsive']//td[contains(text(),'Gender')]//..",
-            birthdateColumn = "//*[@class='table-responsive']//td[contains(text(),'Date of Birth')]//..",
-            mobileColumn = "//*[@class='table-responsive']//td[contains(text(),'Mobile')]//..",
-            subjectsColumn = "//td[text()='Subjects']",
-            hobbyColumn = "//td[text()='Hobbies']",
-            pictureColumn = "//td[text()='Picture']",
-            addressColumn = "//*[@class='table-responsive']//td[contains(text(),'Address')]//..",
-            stateAndCityColumn = "//td[text()='State and City']",
-            closeModal = "#closeLargeModal";
+    public SelenideElement successHeader = $("#example-modal-sizes-title-lg"),
+            nameColumn = $x("//*[contains(text(),'Student Name')]//.."),
+            studentEmailColumn = $x("//*[contains(text(),'Student Email')]//.."),
+            studentGenderColumn = $x("//*[@class='table-responsive']//td[contains(text(),'Gender')]//.."),
+            birthdateColumn = $x("//*[@class='table-responsive']//td[contains(text(),'Date of Birth')]//.."),
+            mobileColumn = $x("//*[@class='table-responsive']//td[contains(text(),'Mobile')]//.."),
+            subjectsColumn = $x("//td[text()='Subjects']"),
+            hobbyColumn = $x("//td[text()='Hobbies']"),
+            pictureColumn = $x("//td[text()='Picture']"),
+            addressColumn = $x("//*[@class='table-responsive']//td[contains(text(),'Address')]//.."),
+            stateAndCityColumn = $x("//td[text()='State and City']"),
+            closeModal = $("#closeLargeModal");
 
     //methods
-    public void setDate() {
+    public void setDate(String monthOfBirth, String yearOfBirth) {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption(monthOfBirth);
         $(".react-datepicker__year-select").click();
--- src/test/java/tests/RegistrationFormWithPageObject.java
+++ src/test/java/tests/RegistrationFormWithPageObject.java
@@ -40,32 +40,32 @@ public class RegistrationFormWithPageObject {
 
         //fill form
         registrationPage.openRegistrationForm();
-        $(registrationPage.name).setValue(firstName);
-        $x(registrationPage.familyName).setValue(lastName);
-        $(registrationPage.userEmail).setValue(email);
-        $x(registrationPage.otherGender).click();
-        $(registrationPage.userNumber).setValue(mobile);
-        registrationPage.setDate();
-        $(registrationPage.subjectInput).val(subject).pressEnter();
-        $(registrationPage.submitButton).scrollIntoView(true);
-        $x(registrationPage.hobbySports).click();
-        $(registrationPage.uploadPicture).uploadFromClasspath(picturePath);
+        registrationPage.name.setValue(firstName);
+        registrationPage.familyName.setValue(lastName);
+        registrationPage.userEmail.setValue(email);
+        registrationPage.otherGender.click();
+        registrationPage.userNumber.setValue(mobile);
+        registrationPage.setDate("August", "1988");
+        registrationPage.subjectInput.val(subject).pressEnter();
+        registrationPage.submitButton.scrollIntoView(true);
+        registrationPage.hobbySports.click();
+        registrationPage.uploadPicture.uploadFromClasspath(picturePath);
         registrationPage.setStateAndCity();
-        $(registrationPage.usersAddress).setValue(currentAddress);
-        $(registrationPage.submitButton).click();
+        registrationPage.usersAddress.setValue(currentAddress);
+        registrationPage.submitButton.click();
 
         //check successful popup
-        $(registrationPage.successHeader).shouldHave(text("Thanks for submitting the form"));
-        $x(registrationPage.nameColumn).shouldHave(text(firstName + ' ' + lastName));
-        $x(registrationPage.studentEmailColumn).shouldHave(text(email));
-        $x(registrationPage.studentGenderColumn).shouldHave(text(gender3));
-        $x(registrationPage.birthdateColumn).shouldHave(text("Date of Birth 15 August,1988"));
-        $x(registrationPage.mobileColumn).shouldHave(text(mobile));
-        $x(registrationPage.subjectsColumn).parent().shouldHave(text(subject));
-        $x(registrationPage.hobbyColumn).parent().shouldHave(text(hobby));
-        $x(registrationPage.pictureColumn).parent().shouldHave(text(pictureName));
-        $x(registrationPage.addressColumn).shouldHave(text(currentAddress));
-        $x(registrationPage.stateAndCityColumn).parent().shouldHave(text(state + ' ' + city));
+        registrationPage.successHeader.shouldHave(text("Thanks for submitting the form"));
+        registrationPage.nameColumn.shouldHave(text(firstName + ' ' + lastName));
+        registrationPage.studentEmailColumn.shouldHave(text(email));
+        registrationPage.studentGenderColumn.shouldHave(text(gender3));
+        registrationPage.birthdateColumn.shouldHave(text("Date of Birth 15 August,1988"));
+        registrationPage.mobileColumn.shouldHave(text(mobile));
+        registrationPage.subjectsColumn.parent().shouldHave(text(subject));
+        registrationPage.hobbyColumn.parent().shouldHave(text(hobby));
+        registrationPage.pictureColumn.parent().shouldHave(text(pictureName));
+        registrationPage.addressColumn.shouldHave(text(currentAddress));
+        registrationPage.stateAndCityColumn.parent().shouldHave(text(state + ' ' + city));
 
     }
 }
